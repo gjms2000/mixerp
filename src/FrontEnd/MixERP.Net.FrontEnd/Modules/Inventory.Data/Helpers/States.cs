f3fd95704a250b127049ffb2111eceebe712a7f5
guru <singh.guru27@gmail.com>
2015-08-19 22:42:16 +0545

    -Refactored party form

--- src/FrontEnd/MixERP.Net.FrontEnd/Modules/Inventory.Data/Helpers/States.cs
+++ src/FrontEnd/MixERP.Net.FrontEnd/Modules/Inventory.Data/Helpers/States.cs
@@ -7,10 +7,10 @@ namespace MixERP.Net.Core.Modules.Inventory.Data.Helpers
 {
     public static class States
     {
-        public static IEnumerable<State> GetStates(string catalog)
+        public static IEnumerable<State> GetStates(string catalog, int countryId)
         {
-            const string sql = "SELECT * FROM core.states;";
-            return Factory.Get<State>(catalog, sql);
+            const string sql = "SELECT * FROM core.states WHERE country_id=@0;";
+            return Factory.Get<State>(catalog, sql, countryId);
         }
     }
 }
