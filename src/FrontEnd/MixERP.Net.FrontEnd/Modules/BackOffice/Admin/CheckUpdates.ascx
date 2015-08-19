b068761be91c6a8beeb38377bb3af0c830dd49e5
binodnp <binodnp@outlook.com>
2015-08-19 22:10:52 +0545

    Fixed #374 "Check Update Page"

---
+++ src/FrontEnd/MixERP.Net.FrontEnd/Modules/BackOffice/Admin/CheckUpdates.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="CheckUpdates.ascx.cs" Inherits="MixERP.Net.Core.Modules.BackOffice.Admin.CheckUpdates" %>
---
+++ src/FrontEnd/MixERP.Net.FrontEnd/Modules/BackOffice/Admin/CheckUpdates.ascx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MixERP.Net.FrontEnd.Base;
+
+namespace MixERP.Net.Core.Modules.BackOffice.Admin
+{
+    public partial class CheckUpdates : MixERPUserControl
+    {
+        public override void OnControlLoad(object sender, EventArgs e)
+        {
+            this.Page.Server.Transfer("/Modules/Update.aspx");
+        }
+    }
+}
